bae402e96d65772ac4b4bb147a36109c84de7792
Andrés Fernando Piedra <panaonchoatu@hotmail.com>
2026-09-20 19:06:33 -0500

    test: add edge cases for word counting

---
+++ src/test/java/com/hacknews/andres/service/WordCounterTest.java
@@ -0,0 +1,41 @@
+package com.hacknews.andres.service;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordCounterTest {
+
+  private final WordCounter wordCounter = new WordCounter();
+
+
+  @ParameterizedTest
+  @CsvSource({
+      "'Hola que-tal como estas', 4",
+      "'Flet 1.0 – Build cross-platform apps in Python', 8",
+      "'Cekura (YC F24) Is Hiring', 5",
+      "'Fujitsu launches made-in-Japan next-generation CPU FUJITSU-MONAKA', 6"
+  })
+  void shouldCountWordsSeparatedByWhitespace(
+      String title,
+      int expected) {
+
+    assertEquals(expected, wordCounter.count(title));
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "'', 0",
+      "'   ', 0",
+      "'Java', 1",
+      "'Java   Spring    Boot', 3",
+      "' Java Spring Boot ', 3"
+  })
+  void shouldHandleWhitespaceCorrectly(
+      String title,
+      int expected) {
+
+    assertEquals(expected, wordCounter.count(title));
+  }
+}
